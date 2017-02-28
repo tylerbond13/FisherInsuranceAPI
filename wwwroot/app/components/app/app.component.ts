a3ce59d9f3fc8901569e6a0cec5fd7eb9cc90f0e
tylerbond13 <tyler.robert.bond@gmail.com>
2017-02-27 23:30:30 -0500

    Lab 5 final commit

---
+++ wwwroot/app/components/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: "fisher-insurance",
+    templateUrl: './app/components/app/app.component.html'
+})
+export class AppComponent { }
